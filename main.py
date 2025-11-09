a49503d2a73e302042a00b785e52ffa5adb8c81f
Mohan Teotia <mohanteotia@Mohans-MacBook-Pro.local>
2025-11-09 22:26:48 +0530

    test2

---
+++ main.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+
+df = pd.read_json('/Users/mohanteotia/Desktop/mini_proj/IDS_NetML/Data/2_training_set.json.gz', lines=True)
+print(df.head())
+print(f"Rows: {df.shape[0]}, Columns: {df.shape[1]}")
